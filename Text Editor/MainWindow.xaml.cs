b6794a1c884b3ab4039fc4eaa4b6cbe6432e20f7
vqasimov12 <q.vaqif1211@gmail.com>
2024-04-23 12:57:54 +0400

    Text editor

---
+++ Text Editor/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System.IO;
+using System.Windows;
+
+namespace Text_Editor;
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void Load_Click(object sender, RoutedEventArgs e)
+    {
+        if (Search.Text == "")
+        {
+            var dialog = new Microsoft.Win32.OpenFileDialog();
+            dialog.FileName = "Document"; 
+            dialog.DefaultExt = ".txt";
+            dialog.Filter = "Text documents|*.txt";
+
+            bool? result = dialog.ShowDialog();
+
+            if (result == true)
+            {
+                var text=File.ReadAllText(dialog.FileName);
+                Richtext.AppendText(text);
+            }
+        }
+    }
+
+    private void Save_Click(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private void Cut_Click(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private void Copy_Click(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private void Paste_Click(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private void Select_Click(object sender, RoutedEventArgs e)
+    {
+
+    }
+}
