d8d399d08c654ff23a4e8cd18e39dc89ffada884
vqasimov12 <q.vaqif1211@gmail.com>
2024-04-26 10:18:51 +0400

    Text editor

--- Text Editor/MainWindow.xaml.cs
+++ Text Editor/MainWindow.xaml.cs
@@ -9,44 +9,67 @@ using System.Windows.Media;
 namespace Text_Editor;
 public partial class MainWindow : Window
 {
-    List<string> FontList = new();
-    List<int> SizeList = new();
+    public List<string> FontList = new();
+    public List<int> SizeList = new();
     string CopyText = "";
+    string filePath = "";
+    bool weight = false;
+    bool italic = false;
+    bool underLine = false;
     public MainWindow()
     {
         InitializeComponent();
-
+        foreach (var item in FontFamily.FamilyNames)
+            FontList.Add(item.ToString());
+        for (int i = 8; i < 50; i++)
+            SizeList.Add(i);
+        Sizes.ItemsSource = SizeList;
+        Sizes.SelectedIndex = 0;
+        Fonts.ItemsSource = FontList;
+        Fonts.SelectedIndex = 0;
     }
     private void Load_Click(object sender, RoutedEventArgs e)
     {
-        if (Search.Text == "")
-        {
-            var dialog = new Microsoft.Win32.OpenFileDialog();
-            dialog.FileName = "Document"; 
-            dialog.DefaultExt = ".txt";
-            dialog.Filter = "Text documents|*.txt";
+        var dialog = new Microsoft.Win32.OpenFileDialog();
+        dialog.FileName = "Document";
+        dialog.DefaultExt = ".txt";
+        dialog.Filter = "Text documents|*.txt";
 
-            bool? result = dialog.ShowDialog();
-
-            if (result == true)
+        if (dialog.ShowDialog() == true)
+            try
             {
-                var text=File.ReadAllText(dialog.FileName);
+                var text = File.ReadAllText(dialog.FileName);
+                Richtext.Document.Blocks.Clear();
                 Richtext.AppendText(text);
+                filePath = dialog.FileName;
+                Search.Text = filePath;
             }
-        }
+            catch (Exception exception)
+            {
+                MessageBox.Show($"{exception.Message}", "Warning", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
     }
 
     private void Save_Click(object sender, RoutedEventArgs e)
     {
-        var dialog = new Microsoft.Win32.SaveFileDialog
+        try
         {
-            FileName = "Document",
-            DefaultExt = ".txt",
-            Filter = "Text documents|*.txt"
-        };
-        bool? result = dialog.ShowDialog();
-        if (result == true)
-            File.WriteAllText(dialog.FileName, new TextRange(Richtext.Document.ContentStart, Richtext.Document.ContentEnd).Text);
+            var dialog = new Microsoft.Win32.SaveFileDialog
+            {
+                FileName = Search.Text == "" ? "Document" : Search.Text,
+                DefaultExt = ".txt",
+                Filter = "Text documents|*.txt"
+            };
+            if (dialog.ShowDialog() == true)
+            {
+                File.WriteAllText(dialog.FileName, new TextRange(Richtext.Document.ContentStart, Richtext.Document.ContentEnd).Text);
+                filePath = dialog.FileName;
+            }
+        }
+        catch (Exception exception)
+        {
+            MessageBox.Show($"{exception.Message}", "Warning", MessageBoxButton.OK, MessageBoxImage.Warning);
+        }
     }
 
     private void Cut_Click(object sender, RoutedEventArgs e)
@@ -63,36 +86,145 @@ public partial class MainWindow : Window
 
     private void Paste_Click(object sender, RoutedEventArgs e)
     {
-       TextPointer insertionPosition = Richtext.CaretPosition;
+        TextPointer insertionPosition = Richtext.CaretPosition;
         insertionPosition.InsertTextInRun(CopyText);
     }
 
-    private void Select_Click(object sender, RoutedEventArgs e)
+    private void Select_Click(object sender, RoutedEventArgs? e)
     {
-        //TextRange range = new TextRange(Richtext.SelectAll, Richtext.End);
-        Richtext.SelectAll(); 
+        Richtext.SelectAll();
         Richtext.SelectionBrush.GetAsFrozen();
     }
 
     private void Bold_Click(object sender, RoutedEventArgs e)
     {
-
+        if (weight == false)
+        {
+            if (Richtext.Selection.Text.Length > 0)
+                Richtext.Selection.ApplyPropertyValue(TextElement.FontWeightProperty, FontWeights.Bold);
+            else
+            {
+                Select_Click(this, null);
+                Richtext.Selection.ApplyPropertyValue(TextElement.FontWeightProperty, FontWeights.Bold);
+            }
+            weight = true;
+        }
+        else
+        {
+            if (Richtext.Selection.Text.Length > 0)
+                Richtext.Selection.ApplyPropertyValue(TextElement.FontWeightProperty, FontWeights.Normal);
+            else
+            {
+                Select_Click(this, null);
+                Richtext.Selection.ApplyPropertyValue(TextElement.FontWeightProperty, FontWeights.Normal);
+            }
+            weight = false;
+        }
     }
-
     private void Italic_Click(object sender, RoutedEventArgs e)
     {
-
+        if (italic == false)
+        {
+            if (Richtext.Selection.Text.Length > 0)
+                Richtext.Selection.ApplyPropertyValue(TextElement.FontStyleProperty, FontStyles.Italic);
+            else
+            {
+                Select_Click(this, null);
+                Richtext.Selection.ApplyPropertyValue(TextElement.FontStyleProperty, FontStyles.Italic);
+            }
+            italic = true;
+        }
+        else
+        {
+            if (Richtext.Selection.Text.Length > 0)
+                Richtext.Selection.ApplyPropertyValue(TextElement.FontStyleProperty, FontStyles.Normal);
+            else
+            {
+                Select_Click(this, null);
+                Richtext.Selection.ApplyPropertyValue(TextElement.FontStyleProperty, FontStyles.Normal);
+            }
+            italic = false;
+        }
     }
-
     private void Underline_Click(object sender, RoutedEventArgs e)
     {
+        if (underLine == false)
+        {
+            if (Richtext.Selection.Text.Length > 0)
+                Richtext.Selection.ApplyPropertyValue(Inline.TextDecorationsProperty, TextDecorations.Underline);
+            else
+            {
+                Select_Click(this, null);
+                Richtext.Selection.ApplyPropertyValue(Inline.TextDecorationsProperty, TextDecorations.Underline);
+            }
+            underLine = true;
+        }
+        else
+        {
+            if (Richtext.Selection.Text.Length > 0)
+                Richtext.Selection.ApplyPropertyValue(Inline.TextDecorationsProperty, null);
+            else
+            {
+                Select_Click(this, null);
+                Richtext.Selection.ApplyPropertyValue(Inline.TextDecorationsProperty, null);
+            }
+            underLine = false;
+        }
 
     }
-
     private void Color_Click(object sender, RoutedEventArgs e)
     {
-        TextRange range = new TextRange(Richtext.Selection.Start, Richtext.Selection.End);
-        range.ApplyPropertyValue(TextElement.ForegroundProperty, Brushes.Red); // Change the color
+        var btn = sender as Button;
+        var brush = btn?.Background as SolidColorBrush;
+        if (brush != null)
+            if (Richtext.Selection.Text.Length > 0)
+            {
+                TextRange range = new TextRange(Richtext.Selection.Start, Richtext.Selection.End);
+                range.ApplyPropertyValue(TextElement.ForegroundProperty, brush);
+            }
+            else
+            {
+                Select_Click(this, null);
+                TextRange range = new TextRange(Richtext.Selection.Start, Richtext.Selection.End);
+                range.ApplyPropertyValue(TextElement.ForegroundProperty, brush);
+            }
+        else
+            MessageBox.Show("Color can not handled correctly", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
 
+    }
+    private void Richtext_TextChanged(object sender, TextChangedEventArgs e)
+    {
+        if (Check.IsChecked == true)
+        {
+            File.WriteAllText(Search.Text, Richtext.ToString());
+        }
+    }
+
+    private void Fonts_SelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+        FontFamily fontFamily = new FontFamily(Fonts.SelectedItem.ToString());
+
+        if (Richtext.Selection.Text.Length == 0)
+            Select_Click(this, null);
+
+        TextRange range = new TextRange(Richtext.Selection.Start, Richtext.Selection.End);
+        range.ApplyPropertyValue(TextElement.FontFamilyProperty, fontFamily);
+    }
+
+    private void Sizes_SelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+        if (Richtext.Selection.Text.Length == 0)
+            Select_Click(null, null);
+        TextRange range = new TextRange(Richtext.Selection.Start, Richtext.Selection.End);
+        range.ApplyPropertyValue(TextElement.FontSizeProperty, Convert.ToDouble(Sizes.SelectedItem));
+    }
+
+    private void Check_Checked(object sender, RoutedEventArgs e)
+    {
+        if (Search.Text.Length == 0)
+        {
+            MessageBox.Show("Save or Load file first", "error", MessageBoxButton.OK, MessageBoxImage.Error);
+            Check.IsChecked = false;
+        }
     }
 }
